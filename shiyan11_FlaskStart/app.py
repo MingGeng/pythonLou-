582770d70a325fd3aed3660632d3998cc5a254d3
MingGeng <gengming8859@icloud.com>
2018-03-25 00:07:42 +0800

    update flask app

--- shiyan11_FlaskStart/app.py
+++ shiyan11_FlaskStart/app.py
@@ -6,9 +6,23 @@ app = Flask(__name__)
 app.config.update({
     'SECTET_KEY': 'a random string'
 })
+
+#app.config.from_pyfile('real_path_of/config.py')
+#app.config.from_envvar(variable_name)
+#app.config.from_object(obj)
+#app.config.from_json(filename)
+#app.config.from_mapping(*mapping, **kwargs)
+
 @app.route('/')
 def index():
-    return 'Hello Flask!'
+    return 'Index'
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post {}'.format(post_id)
+@app.route('/user/<username>')
+def user_index(username):
+    return 'Hello {}!'.format(username)
 
 if __name__ == '__main__':
+    print(app.config['SECRET_KEY'])
     app.run()
