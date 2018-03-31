b26cf14aa1f8b749071c9c7be184d0e5806b885d
MingGeng <gengming8859@icloud.com>
2018-03-31 18:09:21 +0800

    mkdirs

--- auto_echo.sh
+++ auto_echo.sh
@@ -2,13 +2,13 @@
 
 times=540000000
 i=0
-
+list=[2,5,10,15,30,600]
 while [ "$i" -le "$times" ]
 do
     i=$(($i+1))
     echo $i
     echo $i >> echo.echo
-    sleep 5s
+    sleep 10s
     ls ../
 done
 
