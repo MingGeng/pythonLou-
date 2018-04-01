9d6947f98442481fe34e6efff61c1ff6e754986d
MingGeng <gengming8859@icloud.com>
2018-04-01 23:33:25 +0800

    add more jinja2

--- shiyan13_jinja2/app.py
+++ shiyan13_jinja2/app.py
@@ -5,6 +5,15 @@ app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
+def hidden_email(email):
+    parts = email.split('@')
+    parts[0] = '****'
+    return '@'.join(parts)
+
+
+app.add_template_filter(hidden_email)
+
+
 @app.route('/')
 def index():
     teacher = {
