ceb8876595b3684b637a1694e735071c5e06092b
MingGeng <gengming8859@icloud.com>
2018-04-27 21:02:16 +0800

    install virtualenv

--- shiyan18_scrapy_demo_with_DB/shiyanlou/shiyanlou/pipelines.py
+++ shiyan18_scrapy_demo_with_DB/shiyanlou/shiyanlou/pipelines.py
@@ -1,66 +1,67 @@
-# -*- coding: utf-8 -*-
-from sqlalchemy.orm import sessionmaker
-from scrapy.exceptions import DropItem
-from datetime import datetime
-from shiyanlou.models import Course, User, engine
-from shiyanlou.items import CourseItem, UserItem
+# # -*- coding: utf-8 -*-
+# from sqlalchemy.orm import sessionmaker
+# from scrapy.exceptions import DropItem
+# from datetime import datetime
+# from shiyanlou.models import Course, User, engine
+# from shiyanlou.items import CourseItem, UserItem
 
-# # Define your item pipelines here
-# #
-# # Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+# # # Define your item pipelines here
+# # #
+# # # Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# # # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# # class ShiyanlouPipeline(object):
+
+# #     def process_item(self, item, spider):
+# #         item['students'] = int(item['students'])
+# #         if item['students'] < 1000:
+# #             raise DropItem('Course students less than 1000.')
+# #         else:
+# #             self.session.add(Course(**item))
+# #         return item
+
+# #     def open_spider(self, spider):
+# #         Session = sessionmaker(bind=engine)
+# #         self.session = Session()
+
+# #     def close_spider(self, spder):
+# #         self.session.commit()
+# #         self.session.close()
 
 
 # class ShiyanlouPipeline(object):
 
 #     def process_item(self, item, spider):
-#         item['students'] = int(item['students'])
-#         if item['students'] < 1000:
-#             raise DropItem('Course students less than 1000.')
+#         if isinstance(item, CourseItem):
+#             self._process_course_item(item)
+#         elif isinstance(item, UserItem):
+#             self._process_user_item(item)
+#         elif isinstance(item, CourseFollowItem):
+#             self._process_course_follow_item(item)
 #         else:
-#             self.session.add(Course(**item))
+#             pass
 #         return item
+
+#     def _process_course_item(self, item):
+#         item['students'] = int(item['students'])
+#         self.session.add(Course(**item))
+#     def _process_course_follow_item(self, item):
+#         self.session.add()
+
+#     def _process_user_item(self, item):
+#         item['level'] = int(item['level'][1:])
+#         item['join_date'] = datetime.strptime(item['join_date'].split()[0], '%Y-%m-%d').date()
+#         item['learn_courses_num'] = int(item['learn_courses_num'])
+#         self.session.add(User(**item))
 
 #     def open_spider(self, spider):
 #         Session = sessionmaker(bind=engine)
 #         self.session = Session()
 
-#     def close_spider(self, spder):
+#     def close_spider(self, spider):
 #         self.session.commit()
 #         self.session.close()
-
-
-class ShiyanlouPipeline(object):
-
-    def process_item(self, item, spider):
-        if isinstance(item, CourseItem):
-            self._process_course_item(item)
-        elif isinstance(item, UserItem):
-            self._process_user_item(item)
-        elif isinstance(item, CourseFollowItem):
-            self._process_course_follow_item(item)
-        else:
-            pass
-        return item
-
-    def _process_course_item(self, item):
-        item['students'] = int(item['students'])
-        self.session.add(Course(**item))
-    def _process_course_follow_item(self, item):
-#########################        self.session.add(
-    def _process_user_item(self, item):
-        item['level'] = int(item['level'][1:])
-        item['join_date'] = datetime.strptime(item['join_date'].split()[0], '%Y-%m-%d').date()
-        item['learn_courses_num'] = int(item['learn_courses_num'])
-        self.session.add(User(**item))
-
-    def open_spider(self, spider):
-        Session = sessionmaker(bind=engine)
-        self.session = Session()
-
-    def close_spider(self, spider):
-        self.session.commit()
-        self.session.close()
 
 
     
