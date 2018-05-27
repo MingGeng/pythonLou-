1f2e251ae3c0a8ae4f0919c6c68dde793ea7f2fc
MingGeng <gengming8859@icloud.com>
2018-05-27 20:44:34 +0800

    fixed bug of login Page

--- shiyan26/simpledu/simpledu/forms.py
+++ shiyan26/simpledu/simpledu/forms.py
@@ -3,15 +3,15 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import Length, Email, EqualTo, Required
 
 class RegisterForm(FlaskForm):
-    username = StringField('reg_username', validators=[Required(), Length(3, 24)])
+    username = StringField('User Name', validators=[Required(), Length(3, 24)])
     email = StringField('reg_email', validators=[Required(), Email(message='Please input currect email address!')])
     password = PasswordField('reg_password', validators=[Required(), Length(6, 24)])
     repeat_password = PasswordField('reg_repeat_password', validators=[Required(), EqualTo('password')])
     submit = SubmitField('reg_submit')
 
 class LoginForm(FlaskForm):
-    email = StringField('login_email', validators=[Required(), Email(message='Please input currect email address!')])
-    password = PasswordField('login_password', validators=[Required(), Length(6, 24)])
-    remember_me = BooleanField('login_remember_me')
-    submit = SubmitField('login_submit')
+    email = StringField('Email', validators=[Required(), Email(message='Please input currect email address!')])
+    password = PasswordField('Password', validators=[Required(), Length(6, 24)])
+    remember_me = BooleanField('Remember me!')
+    submit = SubmitField('Submit')
 
