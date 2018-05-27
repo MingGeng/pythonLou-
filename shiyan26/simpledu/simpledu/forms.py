622c603e32f6fde2eb130d491d60925aaab9cb2e
MingGeng <gengming8859@icloud.com>
2018-05-27 18:48:49 +0800

    update datas

--- shiyan26/simpledu/simpledu/forms.py
+++ shiyan26/simpledu/simpledu/forms.py
@@ -1,17 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PassworField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import Length, Email, EqualTo, Required
 
 class RegisterForm(FlaskForm):
-    username = StringField('username', validators=[Required(), Length(3, 24)])
-    email = StringField('email', validators=[Required(), Email()])
-    password = PassworField('password', validators=[Required(), Length(6, 24)])
-    repeat_password = PassworField('repeat_password', validators=[Required(), EqualTo('password')])
-    submit = SubmitField('submit')
+    username = StringField('reg_username', validators=[Required(), Length(3, 24)])
+    email = StringField('reg_email', validators=[Required(), Email(message='Please input currect email address!')])
+    password = PasswordField('reg_password', validators=[Required(), Length(6, 24)])
+    repeat_password = PasswordField('reg_repeat_password', validators=[Required(), EqualTo('password')])
+    submit = SubmitField('reg_submit')
 
 class LoginForm(FlaskForm):
-    email = StringField('email', validators=[Required(), Length(6, 24)])
-    password = PassworField('password', validators=[Required(), Length(6, 24)])
-    remember_me = BooleanField('remember_me')
-    submit = SubmitField('submit')
+    email = StringField('login_email', validators=[Required(), Email(message='Please input currect email address!')])
+    password = PasswordField('login_password', validators=[Required(), Length(6, 24)])
+    remember_me = BooleanField('login_remember_me')
+    submit = SubmitField('login_submit')
 
