bb8a3dcc1481686329a0b39f5f1890c02b41c65b
MingGeng <gengming8859@icloud.com>
2018-05-27 21:47:18 +0800

    done with shiyan26

--- shiyan26/simpledu/simpledu/forms.py
+++ shiyan26/simpledu/simpledu/forms.py
@@ -3,11 +3,11 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import Length, Email, EqualTo, Required
 
 class RegisterForm(FlaskForm):
-    username = StringField('User Name', validators=[Required(), Length(3, 24)])
-    email = StringField('reg_email', validators=[Required(), Email(message='Please input currect email address!')])
-    password = PasswordField('reg_password', validators=[Required(), Length(6, 24)])
-    repeat_password = PasswordField('reg_repeat_password', validators=[Required(), EqualTo('password')])
-    submit = SubmitField('reg_submit')
+    username = StringField('User Name:', validators=[Required(), Length(3, 24)])
+    email = StringField('Email:', validators=[Required(), Email(message='Please input currect email address!')])
+    password = PasswordField('Password:', validators=[Required(), Length(6, 24)])
+    repeat_password = PasswordField('Repeat_password:', validators=[Required(), EqualTo('password')])
+    submit = SubmitField('Submit')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[Required(), Email(message='Please input currect email address!')])
