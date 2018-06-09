f402e0b27298ae330f5134397dabf4e3605132a0
MingGeng <gengming8859@icloud.com>
2018-06-10 02:37:09 +0800

    challenge7 done

--- challenge7/news/app.py
+++ challenge7/news/app.py
@@ -1,35 +1,67 @@
 #!/usr/bin/env python3
 
 from flask import Flask, render_template, abort
-import os, json
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/news'
+db = SQLAlchemy(app)
 
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
 
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def  __repr__(self):
+        return '<Category %r>' % self.name
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    created_time = db.Column(db.DateTime)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    content = db.Column(db.Text)
+
+    def __init__(self, id, title, created_time, category_id, content):
+        self.id = id
+        self.title = title
+        self.created_time = created_time
+        self.category_id = category_id
+        self.content = content
+
+    def __repr__(self):
+        return '<Flie %r>' % self.title
+        
 
 @app.route('/')
 def index():
     file_dict = {}
-    path = '/home/shiyanlou/shiyanlou_python_louPlus/challenge6/files'
-    file_list = os.listdir(path)
-    for filename in file_list:
-        with open(os.path.join(path, filename)) as file:
-            file_dict[filename] = json.loads(file.read())
+    files = File.query.all()
+    for file in files:
+        file_dict[file.title] = file
 
     return render_template('index.html', file_dict=file_dict)
 
-@app.route('/files/<filename>')
-def file(filename):
+@app.route('/files/<file_id>')
+def file(file_id):
     file_details = {}
-    filename = filename + '.json'
-    path = '/home/shiyanlou/shiyanlou_python_louPlus/challenge6/files'
-    file_list = os.listdir(path)
-    if filename not in file_list:
-        abort(404)
-    else:
-        with open(os.path.join(path, filename)) as file:
-            file_details = json.loads(file.read())
+    fileid_list = []
+    for file in File.query.all():
+        fileid_list.append(file.id)
+
+    for fileid in fileid_list:
+        if file_id == file_id:
+            file = File.query.filter_by(id=file_id).first()
+            file_details['file_id'] = file.id
+            file_details['created_time'] = file.created_time
+            file_details['content'] = file.content
+            file_details['title'] = file.title
+        else:
+            abort(404)
 
     return render_template('file.html', file_details=file_details)
     
@@ -38,3 +70,7 @@ def file(filename):
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
+
+
+
+
